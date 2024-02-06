345b0aeb83026a1be7800fc1b4e969df5c5ede16
SHREYA ASTHANA <120804051+Shreya-Asthana@users.noreply.github.com>
2024-02-06 18:13:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function showCaption(slide) {
+    const caption = slide.querySelector('.caption');
+    caption.style.display = 'block';
+}
+
+
+function hideCaption(slide) {
+    const caption = slide.querySelector('.caption');
+    caption.style.display = 'none';
+}
